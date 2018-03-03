ef2705d3a92231357c50e40c8a4e58a1e255d4a0
rebel-l <dj@rebel-l.net>
2018-03-03 14:14:19 +0100

    initial go setup to serve static files

---
+++ endpoint/fileserver.go
@@ -0,0 +1,16 @@
+package endpoint
+
+import (
+	"github.com/gorilla/mux"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+)
+
+func InitFileserverEndpoint(r *mux.Router)  {
+	log.Debug("Fileserver endpoint: Init ...")
+
+	fs := http.FileServer(http.Dir("public"))
+	r.Handle("/", http.StripPrefix("/", fs)).Methods(http.MethodGet)
+
+	log.Debug("Fileserver endpoint: initialized!")
+}
---
+++ main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"github.com/rebel-l/salesmanager/endpoint"
+	log "github.com/sirupsen/logrus"
+	"net/http"
+	"strconv"
+)
+
+const port = 3000
+
+func main() {
+	log.Info("Server starting...")
+
+	// Init Router
+	r := mux.NewRouter()
+
+	// Init Endpoints
+	endpoint.InitFileserverEndpoint(r)
+
+	// Serve
+	log.Infof("Listening on port %d ...", port)
+	err := http.ListenAndServe(":" + strconv.Itoa(port), r)
+	if  err != nil {
+		log.Panicf("Couldn't start server. Error: %s", err)
+	}
+}
+
+
